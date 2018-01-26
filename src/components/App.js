142d3f5a2c1c2e9200ffd55ee80e740961175868
Sean Luckett <whitecrane007@gmail.com>
2018-01-26 12:40:07 -0700

    Adds semantic ui for styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+import {Header} from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1 className="App-title">React Photo Gallery</h1>
+        <Header as="h1">React Photo Gallery</Header>
       </div>
     );
   }
